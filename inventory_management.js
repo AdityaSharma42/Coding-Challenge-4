71b7ec47456a28a55c9dad6b4f7e714e0cd1203a
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-15 19:22:47 -0400

    Create calculateInventoryValue function

--- inventory_management.js
+++ inventory_management.js
@@ -6,8 +6,9 @@ const inventory= [
     {name: 'Screen protector', price: 34.99, quantity:15, lowStockLevel: 58},
     {name: 'Pixel buds', price: 199.99, quantity: 175, lowStockLevel: 20},
 ];
-//console.log ('The inventory information is:', inventory)
-// determining whether a product is in stock or has low stock
+//If we want to display the information we can use: console.log ('The inventory information is:', inventory)
+
+//task 2: determining whether a product is in stock or has low stock
 function displayProductDetails(product){//defining the function with the product parameter
     const stockstatus= product.quantity <= product.lowStockLevel? "Low stock": "In Stock";
     //creating a variable to store the result of whether product.quantity is less than or equal to low stock level. Then, I have used a ternary operator to check if something is in stock or low stock
@@ -21,10 +22,9 @@ console.log (displayProductDetails(inventory[0]));
 console.log (displayProductDetails(inventory[1]));
 console.log (displayProductDetails(inventory[2]));
 console.log (displayProductDetails(inventory[3]));
-console.log (displayProductDetails(inventory[4]));
+console.log (displayProductDetails(inventory[4])); // displaying stock levels of all products
 
-// displaying stock levels of all products
-
+//task 3: creating a function to update product stock after sales
 function updateStock(product, unitsSold){//defining updateStock function
 product.quantity -= unitsSold; //subtracting units sold from the number of products in stock
 if (product.quantity<=0){
@@ -41,14 +41,25 @@ updateStock(inventory[2], 52);
 updateStock(inventory[3], 15);
 updateStock(inventory[4], 155);//updating the stock with units sold, which will be subtracted from product quantity.
 
+//task 4: creating a function to check low stock products
 function checkLowStock(inventory){// defining the function with inventory as the parameter
 
  console.log ('The products with a low stock level are:');
- inventory.forEach(product=> {
-    if (product.quantity< product.lowStockLevel){
-        console.log( `${product.name}`); 
+ inventory.forEach(product=> { //usingforEach to iterate over the array
+    if (product.quantity<= product.lowStockLevel){
+        console.log( `${product.name}`);  //if the quantity of the product reaches the low stock level, it the name of the product will be displayed
     }
  });
 }
-checkLowStock(inventory);
+checkLowStock(inventory);//calling the function
+
+// creating a function to calculate total inventory value
+function calculateInventoryValue(inventory){
+    return inventory.reduce((total, product)=>{
+        return total+ (product.price* product.quantity);
+        
+    },0)
+}
+const totalValue= calculateInventoryValue(inventory);
+console.log (`The total inventory value is: ${totalValue}`);
 
