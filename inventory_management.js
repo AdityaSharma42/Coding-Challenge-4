2185c994db17ab3732f1036b2d8c6721f51bec62
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-15 14:50:32 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,7 @@
+let inventory= [
+    {name: 'Pixel 9 Pro',price: 1099.99,quantity: 712,lowstocklevel: 100},
+    {name: 'P9P Clear Clase',price: 29.99,quantity: 467,lowstocklevel: 65},
+    {name: '30W Charger', price: 44.99, quantity: 390,lowstocklevel: 50 },
+    {name: 'Screen protector', price: 34.99, quantity: 415, lowstocklevel: 58},
+    {name: 'Pixel buds', price: 199.99, quantity: 175, lowstocklevel: 20},
+];
